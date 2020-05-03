47887c4325775a0fc7f8f902e6ef6381bc6b316e
purpleWit <dianov-2016@inbox.ru>
2020-05-03 23:29:32 +0300

    Add program math

---
+++ src/Main.java
@@ -0,0 +1,55 @@
+import java.util.Scanner;
+
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        System.out.print("Введите 1 число: ");
+        float first = scanner.nextInt();
+        System.out.print("Введите 2 число: ");
+        float second = scanner.nextInt();
+        Math math = new Math();
+        System.out.println(math.sum(first, second));
+        System.out.println(math.minus(first, second));
+        System.out.println(math.umn(first, second));
+        System.out.println(math.del(first, second));
+        System.out.println(math.ostatok(first, second));
+        System.out.println(math.Pi_numb());
+    }
+}
+
+
+class Math {
+
+    Math () {
+
+    }
+
+    float sum (float x, float y) {
+        return x + y;
+    }
+
+    float minus (float x, float y) {
+        return x - y;
+    }
+
+    float umn (float x, float y) {
+        for (int i = 1; i == y; i++) {
+            x += x;
+        }
+        return x;
+    }
+
+    float ostatok (float x, float y) {
+        return x % y;
+    }
+
+    float del (float x, float y) { // классическое деление
+        return x / y;
+    }
+
+    float Pi_numb () {
+        return 3.14159f;
+    }
+
+}
